b187560fa8c7ae8b99fd02c5580fb261206f1258
Marek Nagy <62247071@cuni.cz>
2022-03-28 13:48:45 +0200

    Refactoring

--- main.py
+++ main.py
@@ -20,6 +20,8 @@ import requests
 # pip only
 import eyed3
 import eyed3.id3.frames
+from pytypes import typechecked
+from rich.progress import track
 
 class InvalidFormatException(Exception):
 	pass
@@ -64,13 +66,10 @@ def get_args() -> Namespace:
 
 	parser.add_argument('--exclude_metadata', dest='add_metadata', action='store_false', help='Exclude metadata (audio only).')
 	parser.set_defaults(add_metadata=True)
-	# parser.add_argument('--max_bitrate', dest='max_bitrate', action='store_true', help='Save the highest bitrate (audio only).')
-	# parser.set_defaults(max_bitrate=False)
 
 	parser.add_argument('--artist', type=str, default=None, help='Add artist metadata')
 	parser.add_argument('--album', type=str, default=None, help='Add album metadata')
 	parser.add_argument('--title', type=str, default=None, help='Add song title metadata')
-	#parser.add_argument('--track', type=int, default= -1, help='Add track number metadata')
 	parser.add_argument('-s', '--silent', dest='silent', action='store_true')
 	parser.set_defaults(silent=False)
 	#parser.add_argument('-s', '--silent', type=bool, default=False, nargs='?', const=True)
@@ -83,9 +82,11 @@ def get_args() -> Namespace:
 	
 	return args
 
+@typechecked
 def get_metadata(yt: YouTube) -> dict:
 	return yt.metadata.metadata[0] if len(yt.metadata.metadata) > 0 else dict()
 
+@typechecked
 def download_video_part(streams: StreamQuery, max_resolution: int, verbose: bool) -> str:
 	max_suitable_resolution = max(
 		filter(
@@ -115,6 +116,7 @@ def download_video_part(streams: StreamQuery, max_resolution: int, verbose: bool
 	res = best_video_stream.download()
 	return res
 
+@typechecked
 def download_audio_part(streams: StreamQuery, verbose: bool) -> str:
 	
 	stream: Stream = streams.order_by('abr').desc().first()
@@ -124,11 +126,13 @@ def download_audio_part(streams: StreamQuery, verbose: bool) -> str:
 	res = stream.download()
 	return res
 
+@typechecked
 def get_compression_preset(compression_level: int) -> str:
 	presets = ['ultrafast', 'superfast', 'veryfast', 'faster', 'fast',
 		'medium', 'slow', 'slower', 'veryslow', 'placebo']
 	return presets[compression_level]
 
+@typechecked
 def remove_forbidden(s: str) -> str:
 	forbidden_symbols = list()
 	if sys.platform == 'win32':
@@ -141,25 +145,153 @@ def remove_forbidden(s: str) -> str:
 	
 	return s
 
+@typechecked
 def on_progress_callback(_, chunk: bytes, bytes_remaining: int, progress_bar: tqdm):
 	progress_bar.update(len(chunk))
 
-def download_video(args: Namespace, yt: YouTube):
-	'''
-	Download video according to the arguments.
-	'''
-	pass
-
-def download_audio(args: Namespace, yt: YouTube):
+@typechecked
+def download_audio(args: Namespace, yt: YouTube, all_streams: StreamQuery, verbose: bool):
 	'''
 	Download audio according to the arguments.
 	'''
-	pass
+	stream = all_streams.filter(type='audio', subtype='webm').order_by('abr').last()
+	stream: Stream
 
-def main():
-	args = get_args()
+	if verbose:
+		print(f'Downloading audio for {yt.title} in {stream.abr}')
+	#out_base = yt.title.replace(" ","_")
+	out_base = remove_forbidden(yt.title)
+	out_ext = stream.mime_type.split("/")[1]
+	out_filename = f'{out_base}.{out_ext}'
+	out_final = f'{out_base}.mp3'
+	#out_filename = remove_forbidden(out_filename) 
+	if out_final not in os.listdir():
+		stream.download(filename=out_filename)
+		
+		# fix file metadata
+		with AudioFileClip(out_filename) as audio_clip:
+			loggerType = 'bar' if verbose else None
+			# remove "bps" from "160kbps" for ffmpeg
+			bitrate = '256k' # f'{stream.abr[:-3]}'
+			audio_clip.write_audiofile(out_final, nbytes=4, bitrate=bitrate, logger=loggerType)
+			#audio_clip.write_audiofile(out_final, logger=None)
+		os.remove(out_filename)
 
-	videos: list[YouTube] = None
+	if (args.add_metadata or args.artist 
+		or args.title or args.album):
+		yt_metadata = get_metadata(yt)
+
+		if args.artist is not None:
+			yt_metadata['Artist'] = args.artist
+		if args.title is not None:
+			yt_metadata['Song'] = args.title
+		if args.title is not None:
+			yt_metadata['Album'] = args.album
+		if verbose:
+			print('Metadata:')
+			for key in yt_metadata:
+				print(f'{key}: {yt_metadata[key]}')
+
+		if len(yt_metadata) == 0:
+			if verbose:
+				print('No metadata found.', file=stderr)
+
+		audio_file = eyed3.load(out_final)
+
+		audio_file.initTag(version=(2, 3, 0))  # version is important for thumbnail
+
+		tag: eyed3.core.Tag = audio_file.tag
+
+		if 'Artist' in yt_metadata:
+			tag.artist = yt_metadata['Artist']
+		else:
+			tag.artist = yt.author
+
+		if 'Song' in yt_metadata:
+			tag.title = yt_metadata['Song']
+		else:
+			tag.title = yt.title
+
+		if 'Album' in yt_metadata:
+			tag.album = yt_metadata['Album']
+		else:
+			tag.album = yt.title
+
+		if verbose:
+			print('Downloading thumbnail...')
+		resp = requests.get(yt.thumbnail_url, stream=True)
+		if resp.status_code == 200:
+			#print(resp.headers)
+			#tag.images.set(eyed3.id3.frames.ImageFrame.FRONT_COVER, resp.content, 'image/jpeg')
+			cover_img = resp.content
+			# with open(f'{ remove_forbidden(yt.title) }_cover.{ resp.headers["Content-Type"].split("/")[-1] }', 'bw') as cover_file:
+			# 	cover_file.write(cover_img)
+			tag.images.set(3, cover_img, 'image/jpg')
+			if verbose:
+				print('Thumbnail has been set.')
+
+		tag.save()
+
+@typechecked
+def download_video(args: Namespace, yt: YouTube, all_streams: StreamQuery, verbose: bool):
+	'''
+	Download video according to the arguments.
+	'''
+	if args.development:
+		if not args.development: # or len(videos) > 1:
+			print('This part is currently under development. Please, use flag -p to download a single file with both audio and video (upto 720p).', file=stderr)
+			#print('Failed to download.', file=stderr)
+			exit(1)
+
+		current_process_id = f'{os.getpid()}_{remove_forbidden(yt.title)}'
+		if verbose:
+			print(f'Creating temporary directory: {current_process_id}')
+
+		# save to temporary directory
+		os.mkdir(str(current_process_id))
+		os.chdir(str(current_process_id))
+		try:
+			audio_file_path = download_audio_part(all_streams.filter(type='audio'), verbose)
+			video_file_path = download_video_part(all_streams.filter(type='video', subtype='mp4').order_by('resolution'), args.max_resolution, verbose)
+
+			title = remove_forbidden(yt.title)
+			final_video_filename = f'{title}.mp4'
+			if verbose:
+				print('Merging video and audio...')
+			with VideoFileClip(video_file_path) as video_clip:
+				with AudioFileClip(audio_file_path) as audio_clip:
+					with video_clip.set_audio(audio_clip) as final_clip:
+						final_clip: VideoFileClip
+						compression_preset = get_compression_preset(args.compress_level)
+						final_clip.write_videofile(final_video_filename, 
+							#codec="h264_nvenc",
+							threads=args.threads, preset=compression_preset, logger='bar' if verbose else None)
+
+			
+			shutil.move(final_video_filename, os.pardir)
+			if verbose:
+				print('Done.')
+		except KeyboardInterrupt:
+			print('\nDownload has been canceled.', file=stderr)
+		# except Exception as e:
+		# 	print(f'The following error occurred:\n{e}', file=stderr)
+		finally:
+			#sleep(1)
+			os.chdir('..')
+			#shutil.rmtree( str(current_process_id) )
+
+	# progressive -> video and audio are in one file together (max 720p)
+	else:
+		best_stream: Stream = all_streams.filter(type='video', progressive=True, subtype='mp4').order_by('resolution').last()
+		filename = remove_forbidden(yt.title + '.' + best_stream.mime_type.split('/')[1])
+		if filename in os.listdir():
+			return
+		if verbose:
+			print(f'Downloading {yt.title}...', file=stderr)
+		best_stream.download(filename=filename)
+
+@typechecked
+def get_urls(args: Namespace) -> list[YouTube]:
 	if args.playlist is not None:
 		try:
 			playlist = Playlist(args.playlist)
@@ -191,153 +323,34 @@ def main():
 			print(f'Failed to download media from {args.url}', file=stderr)
 			exit(1)
 
+@typechecked
+def progress_update(iterator: Iterable, yt: YouTube):
+	iterator.desc = f'{yt.title[:35]}'
+	iterator.refresh()
+
+def main():
+	args = get_args()
+
+	videos: list[YouTube] = get_urls()
+
 	verbose: bool = not args.silent and len(videos) == 1
 	iterator: Iterable = tqdm(videos, ascii=True) if len(videos) > 1 else videos
 	try:
 		for yt in iterator:
 			yt: YouTube
+			
 			if len(videos) > 1:
-				iterator.desc = f'{yt.title[:35]}'
-				iterator.refresh()
+				progress_update(iterator, yt)
+
 			try:
 				all_streams = yt.streams
 			except MembersOnly: # video in the playlist is only for members of the channel
 				continue
 
 			if args.format in [Format.Video, Format.Both]:
-				if args.development:
-					if not args.development: # or len(videos) > 1:
-						print('This part is currently under development. Please, use flag -p to download a single file with both audio and video (upto 720p).', file=stderr)
-						#print('Failed to download.', file=stderr)
-						exit(1)
-
-					current_process_id = f'{os.getpid()}_{remove_forbidden(yt.title)}'
-					if verbose:
-						print(f'Creating temporary directory: {current_process_id}')
-
-					# save to temporary directory
-					os.mkdir(str(current_process_id))
-					os.chdir(str(current_process_id))
-					try:
-						audio_file_path = download_audio_part(all_streams.filter(type='audio'), verbose)
-						video_file_path = download_video_part(all_streams.filter(type='video', subtype='mp4').order_by('resolution'), args.max_resolution, verbose)
-
-						title = remove_forbidden(yt.title)
-						final_video_filename = f'{title}.mp4'
-						if verbose:
-							print('Merging video and audio...')
-						with VideoFileClip(video_file_path) as video_clip:
-							with AudioFileClip(audio_file_path) as audio_clip:
-								with video_clip.set_audio(audio_clip) as final_clip:
-									final_clip: VideoFileClip
-									compression_preset = get_compression_preset(args.compress_level)
-									final_clip.write_videofile(final_video_filename, codec="h264_nvenc",
-										threads=args.threads, preset=compression_preset, logger='bar' if verbose else None)
-
-						
-						shutil.move(final_video_filename, os.pardir)
-						if verbose:
-							print('Done.')
-					except KeyboardInterrupt:
-						print('\nDownload has been canceled.', file=stderr)
-					# except Exception as e:
-					# 	print(f'The following error occurred:\n{e}', file=stderr)
-					finally:
-						sleep(1)
-						os.chdir('..')
-						shutil.rmtree( str(current_process_id) )
-
-				# progressive -> video and audio are in one file together (max 720p)
-				else:
-					best_stream: Stream = all_streams.filter(type='video', progressive=True, subtype='mp4').order_by('resolution').last()
-					filename = remove_forbidden(yt.title + '.' + best_stream.mime_type.split('/')[1])
-					if filename in os.listdir():
-						continue
-					if verbose:
-						print(f'Downloading {yt.title}...', file=stderr)
-					best_stream.download(filename=filename)
+				download_video(args, yt, all_streams, verbose)
 			if args.format in [Format.Audio, Format.Both]:
-				stream = all_streams.filter(type='audio', subtype='webm').order_by('abr').last()
-				stream: Stream
-
-				if verbose:
-					print(f'Downloading audio for {yt.title} in {stream.abr}')
-				#out_base = yt.title.replace(" ","_")
-				out_base = remove_forbidden(yt.title)
-				out_ext = stream.mime_type.split("/")[1]
-				out_filename = f'{out_base}.{out_ext}'
-				out_final = f'{out_base}.mp3'
-				#out_filename = remove_forbidden(out_filename) 
-				if out_final not in os.listdir():
-					stream.download(filename=out_filename)
-					
-					# fix file metadata
-					with AudioFileClip(out_filename) as audio_clip:
-						loggerType = 'bar' if verbose else None
-						# remove "bps" from "160kbps" for ffmpeg
-						bitrate = '256k' # f'{stream.abr[:-3]}'
-						audio_clip.write_audiofile(out_final, nbytes=4, bitrate=bitrate, logger=loggerType)
-						#audio_clip.write_audiofile(out_final, logger=None)
-					os.remove(out_filename)
-
-				if (args.add_metadata or args.artist 
-					or args.title or args.album):
-					yt_metadata = get_metadata(yt)
-
-					if args.artist is not None:
-						yt_metadata['Artist'] = args.artist
-					if args.title is not None:
-						yt_metadata['Song'] = args.title
-					if args.title is not None:
-						yt_metadata['Album'] = args.album
-					if verbose:
-						print('Metadata:')
-						for key in yt_metadata:
-							print(f'{key}: {yt_metadata[key]}')
-
-					if len(yt_metadata) == 0:
-						if verbose:
-							print('No metadata found.', file=stderr)
-
-					audio_file = eyed3.load(out_final)
-
-					audio_file.initTag(version=(2, 3, 0))  # version is important for thumbnail
-
-					tag: eyed3.core.Tag = audio_file.tag
-
-					if 'Artist' in yt_metadata:
-						tag.artist = yt_metadata['Artist']
-					else:
-						tag.artist = yt.author
-
-					if 'Song' in yt_metadata:
-						tag.title = yt_metadata['Song']
-					else:
-						tag.title = yt.title
-
-					if 'Album' in yt_metadata:
-						tag.album = yt_metadata['Album']
-					else:
-						tag.album = yt.title
-
-					if verbose:
-						print('Downloading thumbnail...')
-					resp = requests.get(yt.thumbnail_url, stream=True)
-					if resp.status_code == 200:
-						#print(resp.headers)
-						#tag.images.set(eyed3.id3.frames.ImageFrame.FRONT_COVER, resp.content, 'image/jpeg')
-						cover_img = resp.content
-						# with open(f'{ remove_forbidden(yt.title) }_cover.{ resp.headers["Content-Type"].split("/")[-1] }', 'bw') as cover_file:
-						# 	cover_file.write(cover_img)
-						tag.images.set(3, cover_img, 'image/jpg')
-						if verbose:
-							print('Thumbnail has been set.')
-
-					tag.save()
-		# if len(videos) > 1:
-		# 	for obj in os.listdir():
-		# 		if os.path.isdir(obj) and obj.startswith(f'{os.getpid()}'):
-		# 			shutil.rmtree(obj)
+				download_audio(args, yt)
 	except KeyboardInterrupt:
 		print('Your download has been canceled.')
 		exit(1)
